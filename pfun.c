b487d3c16ef05cfb3af222c28e843c6ffe7fe88a
haselby <jeffrey.haselby@gmail.com>
2018-02-13 12:06:41 -0500

    Added pfun.c
    Pointer example using strings

---
+++ pfun.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    char *mystr, *m;
+    mystr = (char*)malloc(1000); //allocate memory for 999 characters and one centinal character
+    printf("Enter string here: ");
+//    scanf("%s", mystr); // will read up through first space
+    scanf("%[^\n]s",mystr); // will read until newline character hit
+    m = mystr; // point m at same pointee as mystr (same address in memory)
+    printf("%s\n",mystr);
+    printf("%s\n",m);
+    free(mystr);
+}
